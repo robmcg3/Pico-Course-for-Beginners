a8bc3e7b837855ba3e2fda228e5d4f5d926cb183
robmcg3 <robmcg3@hotmail.com>
2025-08-20 14:25:52 -0400

    command line commit 2

---
+++ analog_input.py
@@ -0,0 +1,16 @@
+from machine import Pin, ADC # we need to import ADC to use analog inputs
+import time
+
+# set up pin 26 as an analog input
+pot = ADC(Pin(26)) 
+
+# constant to convert the 0 - 65535 range to 0 - 3.3 volts
+conversion_factor = 3.3 / 65535
+
+while True:
+    # read analog input and store it in a variable called pot_voltage
+    pot_voltage = pot.read_u16()
+  
+    # print pot_voltage
+    print(pot_voltage)
+    time.sleep(0.1)
