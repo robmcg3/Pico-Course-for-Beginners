ddb0e3cf2304b4cf1150a1a71b74f5074540a567
robmcg3 <robmcg3@hotmail.com>
2025-08-20 14:09:34 -0400

    command line commit

---
+++ button_LED.py
@@ -0,0 +1,17 @@
+from machine import Pin
+import time
+
+# set up pin 15 as an input with a pull-down resistor
+button = Pin(14, Pin.IN, Pin.PULL_DOWN)
+# set up pin 16 connected to the LED as an output
+led = Pin(15, Pin.OUT)
+
+
+while True:
+    # if the button is pressed, then turn on the LED
+    if button.value() == 1:
+        led.value(1)
+    
+    # if its not pressed, then turn the LED off
+    else:
+        led.value(0)
